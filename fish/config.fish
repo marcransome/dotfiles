a0b2dbe1b43cf5ee6cf6d8e41ce2e22ad6255385
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2013-08-29 20:43:42 +0100

    adding fish config

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+# set path ordering to favour homebrew 
+set -x PATH /usr/local/bin /usr/local/sbin ~/bin /usr/local/opt/ruby/bin $PATH
+
+# show current git branch in prompt
+set __fish_git_prompt_color_branch yellow
