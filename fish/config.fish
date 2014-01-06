db33c0f45befb31964dbd8f89dc6181136da049e
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2014-01-06 13:38:34 +0000

    adding alias for coloured grep output

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,6 @@ set -x PATH /usr/local/bin /usr/local/sbin ~/bin /usr/local/opt/ruby/bin $PATH
 
 # show current git branch in prompt
 set __fish_git_prompt_color_branch yellow
+
+# configure some convenient aliases
+alias grep="grep --color=always"
