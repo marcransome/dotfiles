bf4ecf4a98a61cb4eae1e29d4200603488359e80
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2014-03-06 12:02:57 +0000

    updating jekyll post function to include basic front-matter

--- fish/functions/jp.fish
+++ fish/functions/jp.fish
@@ -1,19 +1,22 @@
-function jp -d "Open default editor with date prepended filename for jekyll"
+function jp -d "Open default editor with date prepended filename and specified title for jekyll posts."
 
-	set title (date +"%Y-%m-%d")
-        
-        switch (count $argv)
-                case 0
-                        echo "No post title was specified."
-                        return 1
-                case 1
-                        set title $title-$argv[1].md
-                case \*
-                        for arg in $argv
-                                set title $title-$arg
-                        end
-                        set title $title.md
-        end
-        
-        EDITOR $title
+  set title (date +"%Y-%m-%d")
+  set front_matter "---\nlayout:\ntitle:\nimage:\n---\n"
+
+  switch (count $argv) 
+    case 0
+      echo "No post title was specified."
+      return 1
+    case 1
+      set lower_arg (echo $argv[1] | tr A-Z a-z)
+      set title $title-$lower_arg.md
+    case \*
+      for arg in $argv
+        set lower_arg (echo $arg | tr A-Z a-z)
+        set title $title-$lower_arg
+      end
+      set title $title.md
+  end
+
+  echo -e $front_matter | EDITOR - +"file $title"
 end
