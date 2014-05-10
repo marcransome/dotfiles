13239b20164730ac2dc5f5995c1d76bf97c9ecf8
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2014-05-10 15:02:11 +0100

    simplifying description for jekyll post function and updating soft tabs for consistency

--- fish/functions/jp.fish
+++ fish/functions/jp.fish
@@ -1,22 +1,21 @@
-function jp -d "Open default editor with date prepended filename and specified title for jekyll posts."
+function jp -d "Open editor with suitable filename for jekyll posts."
+    set title (date +"%Y-%m-%d")
+    set front_matter "---\nlayout: post\ntitle:\nimage:\n---\n"
 
-  set title (date +"%Y-%m-%d")
-  set front_matter "---\nlayout: post\ntitle:\nimage:\n---\n"
+    switch (count $argv) 
+        case 0
+            echo "No post title was specified."
+            return 1
+        case 1
+            set lower_arg (echo $argv[1] | tr A-Z a-z)
+            set title $title-$lower_arg.md
+        case \*
+            for arg in $argv
+                set lower_arg (echo $arg | tr A-Z a-z)
+                set title $title-$lower_arg
+            end
+            set title $title.md
+    end
 
-  switch (count $argv) 
-    case 0
-      echo "No post title was specified."
-      return 1
-    case 1
-      set lower_arg (echo $argv[1] | tr A-Z a-z)
-      set title $title-$lower_arg.md
-    case \*
-      for arg in $argv
-        set lower_arg (echo $arg | tr A-Z a-z)
-        set title $title-$lower_arg
-      end
-      set title $title.md
-  end
-
-  echo -e $front_matter | EDITOR - +"file $title"
+    echo -e $front_matter | EDITOR - +"file $title"
 end
